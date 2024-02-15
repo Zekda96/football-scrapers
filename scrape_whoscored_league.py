d014c4b478a43c51a34d5e19a8963d170b72447b
Zekda-PC <danielgranjac@gmail.com>
2024-02-15 18:23:24 -0500

    added list of match IDs from schedule to scrape whole season

--- scrape_whoscored_league.py
+++ scrape_whoscored_league.py
@@ -1,10 +1,10 @@
 # Get WhoScored event data for a whole season
 import soccerdata as sd
 
-league = 'ENG-Premier League'
+# league = 'ENG-Premier League'
 # league = 'ESP-La Liga'
 # league = 'GER-Bundesliga'
-# league =  'ITA-Serie A'
+league = 'ITA-Serie A'
 # league =  'FRA-Ligue 1'
 # league = 'INT-Women's World Cup'
 # league = 'INT-World Cup'
@@ -16,8 +16,8 @@ league = 'ENG-Premier League'
 #           ]
 
 # season = '21-22'
-season = '22-23'
-# season = '23-24'
+# season = '22-23'
+season = '23-24'
 
 ws = sd.WhoScored(leagues=league, seasons=season)
 # ws = sd.WhoScored(leagues=league, seasons=season,
@@ -25,7 +25,7 @@ ws = sd.WhoScored(leagues=league, seasons=season)
 #                   no_store=True,
 #                   headless=True)
 
-schedule = ws.read_schedule(force_cache=True)
+schedule = ws.read_schedule()
 # schedule = ws.read_schedule()
 matchs_no = len(schedule)
 
@@ -33,18 +33,10 @@ matchs_no = len(schedule)
 # match_id = 1729468
 match_id = ''
 
-if match_id != '':
-    # Load one match
-    events = ws.read_events(match_id=1729468, force_cache=True)
 if match_id == '':
-    # Loop to load season
-    for i in range(matchs_no):
-        percent = (i + 1) / matchs_no * 100
-        print(f'Reading match Number {i + 1}/380 - {round(percent, 2)} %')
-        # Events get saved on "~/soccerdata/data/WhoScored" automatically
-        events = ws.read_events(
-            match_id=int(schedule.iloc[[i]].game_id.values[0]),
-            force_cache=True
-        )
-        print('Got match event data\nSaving to list...')
-
+    events = ws.read_events(
+        match_id=schedule['game_id'].values.tolist(),
+    )
+else:
+    # Load one match
+    events = ws.read_events(match_id=int(match_id))
