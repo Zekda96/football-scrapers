c69ea8e4f703d82f51651ec5fa1dd4b128a87391
Zekda-PC <danielgranjac@gmail.com>
2025-02-25 15:17:40 -0500

    Added all qualifier columns

--- supabase-project/clean_dataframe.py
+++ supabase-project/clean_dataframe.py
@@ -1,5 +1,27 @@
 from datetime import datetime, timezone
 import pandas as pd
+from re import sub
+
+pd.options.mode.copy_on_write = True
+
+def to_snake_case(s: str):
+  return '_'.join(
+    sub('([A-Z][a-z]+)', r' \1',
+    sub('([A-Z]+)', r' \1',
+    s.replace('-', ' '))).split()).lower()
+
+
+def transform_json(json_list: list):
+    """
+    Transforms a list of dictionaries into a single dictionary.
+    """
+    result = {}
+    for item in json_list:
+        display_name = to_snake_case(item['type']['displayName'])
+        value = item.get('value', None)  # Get the value, default to None if not present
+        result[display_name] = value
+    return result
+
 
 def clean_df(dataframe: pd.DataFrame):
     """
@@ -53,4 +75,39 @@ def clean_df(dataframe: pd.DataFrame):
 
     df["sent_timestamp"] = pd.Timestamp(datetime.timestamp(datetime.now(timezone.utc)), unit='s', tz='UTC')
 
-    return df
+    # Add relevant qualifiers as new columns
+
+    #Load the qualifiers
+    dfq = pd.read_csv("qualifiers.csv", names=["num", "name", "value", "type", "description", "qualifier"])
+    qualifiers = dfq["name"].to_list()
+    qualifiers = [to_snake_case(q) for q in qualifiers]
+
+    # Fill missing values with NaN (optional, pandas does this by default)
+    og_cols = df.columns.to_list()
+
+    required_columns = og_cols + qualifiers
+
+    # Apply(json.loads) to convert the JSON strings into dictionaries and create a new DataFrame
+    transformed_data = df["qualifiers"].apply(transform_json)
+
+    # Convert the list of dictionaries into a DataFrame
+    result_df = pd.DataFrame(transformed_data.tolist())
+
+    # Concatenate with the original DataFrame if needed
+    df = pd.concat([df, result_df], axis=1)
+
+    # Create a new DataFrame with all required columns, filling missing ones with pd.NA
+    final_df = pd.DataFrame(columns=required_columns)
+
+    # Fill the final DataFrame with values from result_df
+    for col in required_columns:
+        try:
+            final_df[col] = df.get(col, pd.NA)
+
+        except ValueError as e:
+            # Repeated columns
+            final_df[col] = df.get(col).iloc[:,0]
+
+    final_df.pop("qualifiers")
+
+    return final_df
